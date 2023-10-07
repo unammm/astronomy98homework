3ee426dfb338e3598f1db539dd7c7b3dfa8ac63f
Una Milincic <una.milincic@gmail.com>
2023-10-07 14:01:51 -0700

    hw4 resubmission final

---
+++ hw3.py
@@ -0,0 +1,29 @@
+# PROBLEM 1
+"""
+def power(input, power):
+    input1 = 1
+    for i in range(power):
+        input1 = input1 * input
+    print(input1)
+    return [input1]
+"""
+"""
+input1 or input' = 1 since input^0 will ALWAYS be 1 and it's actually 
+x^y = 1 * input * input * input * input, meaning that input1 for first i 
+is accounted for. 
+"""
+
+# PROBLEM 2
+def minandmax(list1):
+   for i in range (len(list1)):
+      num = list[i]
+      if num > list[i-1]: 
+         
+
+      min = 
+      tupleminmax = (num for num )
+    print(tupleminmax)
+   list1 = [5000, 600, 70, 15432]
+   minandmax(list1)
+   print(tupleminmax)
+   return [minandmax(list1)]
